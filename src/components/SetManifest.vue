ed45d731f91c1ff7ca76ab4577a02133870f268e
alvcode <alvcode@ya.ru>
2025-03-02 16:51:25 +0300

    manifest

---
+++ src/components/SetManifest.vue
@@ -0,0 +1,76 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+
+export default {
+  name: "SetManifest",
+  components: {},
+  data() {
+    return {
+      
+    };
+  },
+  props: {
+    
+  },
+  watch: {
+    
+  },
+  
+  methods: {
+    setManifest(){
+      let manifestJSON =
+      {
+        "name": "Assistant",
+        "short_name": "Assistant",
+        "scope": window.location.protocol +'//' +window.location.host,
+        "start_url": window.location.protocol +'//' +window.location.host,
+        "display": "standalone",
+        "icons":[
+            {
+                "src":"/img/icons/android-chrome-192x192.png",
+                "sizes":"192x192",
+                "type":"image/png"
+            },
+            {
+              "src":"/img/icons/android-chrome-512x512.png",
+              "sizes":"192x192",
+              "type":"image/png"
+            },
+            {
+                "src":"/img/icons/apple-touch-icon.png",
+                "sizes":"180x180",
+                "type":"image/png"
+            },
+        ],
+        themeColor: "#38424e",
+        theme_color: "#38424e",
+        msTileColor: "#000",
+        background_color: "#38424e",
+        backgroundColor: "#38424e",
+        manifestOptions: {
+          background_color: "#38424e",
+          backgroundColor: "#38424e",
+          themeColor: "#38424e",
+          theme_color: "#38424e",
+        }
+      };
+
+      const stringManifest = JSON.stringify(manifestJSON);
+      const blob = new Blob([stringManifest], {type: 'application/json'});
+      const manifestURL = URL.createObjectURL(blob);
+      document.querySelector('link[rel="manifest"]').setAttribute('href', manifestURL);
+      document.querySelector('meta[name="theme-color"]').setAttribute('content', '#38424e');
+    }
+  },
+  created() {
+    this.setManifest();
+  }
+};
+</script>
+
+<style scoped lang="less">
+
+</style>
--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,43 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    VitePWA({
+      manifest: {
+        name: 'Assistant',
+        short_name: 'Assistant',
+        scope: '/',
+        start_url: '/',
+        display: 'standalone',
+        icons: [
+          {
+            src: '/img/icons/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/img/icons/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: '/img/icons/apple-touch-icon.png',
+            sizes: '180x180',
+            type: 'image/png',
+          },
+        ],
+        theme_color: '#38424e',
+        background_color: '#38424e',
+      },
+      workbox: {
+        // Настройки Workbox (опционально)
+      },
+    }),
   ],
   server: {
     port: 5173
